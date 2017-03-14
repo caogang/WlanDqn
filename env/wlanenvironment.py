fc28f530f5e3335b2fae60c7cfae898d22e46317
MarvinCAO-MAC <caogang440@163.com>
2017-03-14 09:22:19 +0800

    完成与决策大脑之间接口的设计和实现

--- env/wlanenvironment.py
+++ env/wlanenvironment.py
@@ -1,6 +1,8 @@
 import threading
 import urllib2
 from time import sleep
+import numpy as np
+import json
 
 class wlanEnv:
     def __init__(self, remoteControllerAddr, seqLen, timeInterval):
@@ -9,22 +11,76 @@ class wlanEnv:
         self.seqLen = seqLen
         self.timeInverval = timeInterval
         self.threads = []
-        self.end = False;
+        self.end = False
 
-    def getStatesFromRemote(self, clientHwAddr, timeInterval):
+
+        self.macAddr = '94:65:9c:84:a3:32'
+        rssiUrl = 'http://' + self.remoteAddr + "/dqn/rssi/json?mac=" + self.macAddr
+        rssiDict = curl_keystone(rssiUrl)
+        rssiDict = json.loads(rssiDict)
+        dictKey = rssiDict.keys()
+        dictKey.remove('state')
+        self.numAp = len(dictKey)
+        # self.ap2id = dict(zip(dictKey, xrange(0, self.numAp)))
+        self.id2ap = dict(zip(xrange(0, self.numAp), dictKey))
+        self.obsevation = None
+        self.valid = False
+
+    def __getStatesFromRemote(self, clientHwAddr, timeInterval):
         while not self.end:
-            rssiUrl = 'http://' + self.remoteAddr + "/dqn/rssi/json?mac=" + clientHwAddr
+            rssiUrl = 'http://' + self.remoteAddr + '/dqn/rssi/json?mac=' + clientHwAddr
             rssiDict = curl_keystone(rssiUrl)
-            rewardUrl = 'http://' + self.remoteAddr + "/dqn/reward/json?mac=" + clientHwAddr
+            rssiDict = json.loads(rssiDict)
+            rewardUrl = 'http://' + self.remoteAddr + '/dqn/reward/json?mac=' + clientHwAddr
             rewardDict = curl_keystone(rewardUrl)
-            print 'rssi'
-            print rssiDict
-            print 'reward'
-            print rewardDict
+            rewardDict = json.loads(rewardDict)
+            # print 'rssi'
+            # print rssiDict
+            # print 'reward'
+            # print rewardDict
+            if rssiDict['state'] and rewardDict['state']:
+                rssiDict.pop('state')
+                rewardDict.pop('state')
+                self.reward = rewardDict['reward']
+                if self.obsevation is None :
+                    self.obsevation = np.array([rssiDict.values()])
+                elif self.obsevation.shape[0] == self.seqLen:
+                    obsevation = np.delete(self.obsevation, (0), axis=0)
+                    obsevation = np.append(obsevation, [rssiDict.values()],axis=0)
+                    self.obsevation = obsevation
+                    if not self.valid:
+                        self.valid = True
+                else:
+                    self.obsevation = np.append(self.obsevation, [rssiDict.values()], axis=0)
             sleep(timeInterval)
 
+    def __handover(self, clientHwAddr, agentIp):
+        handoverUrl = 'http://' + self.remoteAddr + '/dqn/handover/json?mac=' + clientHwAddr + '&&agent=' + agentIp
+        print handoverUrl
+
+    '''
+    @:returns
+    input vector dimension
+    action space dimension
+    '''
+    def getDimSpace(self):
+        return self.numAp, self.numAp + 1
+
+    def observe(self):
+        rssi = self.obsevation.astype(int)
+        return self.valid, rssi
+
+    def step(self, actionId):
+        if actionId >= self.numAp:
+            return
+        self.__handover(self.macAddr, self.id2ap[actionId])
+        return
+
+    def getReward(self):
+        return self.valid, self.reward
+
     def start(self):
-        t1 = threading.Thread(target=self.getStatesFromRemote, args=('94:65:9c:84:a3:32', self.timeInverval))
+        t1 = threading.Thread(target=self.__getStatesFromRemote, args=(self.macAddr, self.timeInverval))
         self.threads.append(t1)
         for t in self.threads:
             t.setDaemon(True)
@@ -43,9 +99,20 @@ def curl_keystone(url):
     return response.read()
 
 if __name__ == '__main__':
-    env = wlanEnv('10.103.12.166:8080', 10, timeInterval=1)
+    env = wlanEnv('10.103.12.166:8080', 10, timeInterval=0.1)
     env.start()
-    sleep(5)
+    sleep(2)
+    print env.observe()
+    print env.step(0)
+    sleep(0.1)
+    print env.observe()
+    print env.step(0)
+    sleep(0.1)
+    print env.observe()
+    print env.step(0)
+    sleep(0.1)
+    print env.step(1)
+    print env.getDimSpace()
     env.stop()
     sleep(2)
     pass
