af2aab1c2f447de799bb677e5e306d21e44a6df2
MarvinCAO-Portal <caogang440@163.com>
2017-03-28 16:41:53 +0800

    修改时间维度为60*100ms=6s，并且开始使用预训练参数

--- main.py
+++ main.py
@@ -5,14 +5,14 @@ import numpy as np
 
 CONTROLLER_IP = '10.103.12.166:8080'
 BUFFER_LEN = 60
-ENV_REFRESH_INTERVAL = 0.01
+ENV_REFRESH_INTERVAL = 0.1
 
 def main():
     env = wlanEnv(CONTROLLER_IP, BUFFER_LEN, timeInterval=ENV_REFRESH_INTERVAL)
     env.start()
 
     numAPs, numActions, numAdditionDim = env.getDimSpace()
-    brain = BrainDQN(numActions, numAPs, numAdditionDim, BUFFER_LEN)
+    brain = BrainDQN(numActions, numAPs, numAdditionDim, BUFFER_LEN, param_file='saved_networks/network-dqn.params')
 
     while not env.observe()[0]:
         time.sleep(0.5)
